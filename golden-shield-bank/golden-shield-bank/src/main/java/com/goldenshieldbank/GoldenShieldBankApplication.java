7821dda83dc587b6ddca4df97be668007a456972
PedroMoreiraDev <pedrodev2020@gmail.com>
2023-11-06 15:20:27 -0300

    creating the email service and implementing it when creating an account

---
+++ golden-shield-bank/golden-shield-bank/src/main/java/com/goldenshieldbank/GoldenShieldBankApplication.java
@@ -0,0 +1,13 @@
+package com.goldenshieldbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldenShieldBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoldenShieldBankApplication.class, args);
+	}
+
+}
---
+++ golden-shield-bank/golden-shield-bank/src/test/java/com/goldenshieldbank/GoldenShieldBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goldenshieldbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldenShieldBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
